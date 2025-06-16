4929587f40ea4f20aa1039cf8bcbb5b0f953774d
juanma <97960523+Coladero@users.noreply.github.com>
2025-06-16 18:17:31 +0100

    Fix: Update vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import vercel from 'vite-plugin-vercel'
 
 export default defineConfig({
-  plugins: [react()],
-  base:'./',
+  plugins: [
+    react(),
+    vercel()
+  ],
+  server: {
+    port: Number(process.env.PORT) || 3000
+  }
 })
